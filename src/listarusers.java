86b3ae887d4298cea52bae495ef1b741f2656278
Caio <breckn4444@gmail.com>
2026-07-21 22:59:14 -0300

    Terminando o cad

--- src/listarusers.java
+++ src/listarusers.java
@@ -1,3 +1,22 @@
-public class listarusers {
+import java.util.ArrayList;
+import java.util.List;
+public class listarusers extends cadusuario {
+    // primeiro passo - cadastrar o usuario
+    List <cadusuario> listadeusers = new ArrayList<>();
+    cadusuario User = new cadusuario();
 
+
+
+
+
+    // Geteers and seeters
+    public List<cadusuario> getListadeusers() {
+
+        return listadeusers;
+    }
+
+    public void setListadeusers(List<cadusuario> listadeusers) {
+
+        this.listadeusers = listadeusers;
+    }
 }
